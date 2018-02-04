abcb8aa61cd3776fc74ed54adba86cd384083958
rchaser53 <tayoshizawa29@gmail.com>
2018-02-04 15:56:48 +0900

    memorize to use 'gulp generic' pdf.js build

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 curl -L https://github.com/mozilla/pdf.js/archive/gh-pages.zip > pdf.js-gh-pages.zip
+
+
+git clone https://github.com/mozilla/pdf.js.git
+
+build/generic/web/viewer.html
+perhaps need to change gulp task command(i don't wanna it)
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const server = require('http').createServer(app)
 app.use('/pdf.js-gh-pages', express.static(path.join(__dirname, 'pdf.js-gh-pages')));
 app.use('/pdf', express.static(path.join(__dirname, 'pdf')));
 app.use('/src', express.static(path.join(__dirname, 'src')));
+app.use('/build', express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
